0a6962b92e00c5fd5aa7d8e611dea3c7117afec3
brightmanforever022 <brightmanforever@outlook.com>
2015-02-16 17:21:35 -0500

    Updated Push Transaction Module
    pushtx module implemented fully

--- blockchain.info/pushtx.js
+++ blockchain.info/pushtx.js
@@ -6,13 +6,23 @@ var appendToURL = helper.appendToURL;
 
 var pushtx = {};
 
+// [untested]
 pushtx.pushtx = function(tx, apiCode, callback) {
+
+	var payload = {
+		'tx': tx
+	}
 
 	if (arguments.length === 2) {
 		callback = apiCode;
 		apiCode = undefined;
+	} else {
+		payload['api_code'] = apiCode;
 	}
 
+	var url = root + 'pushtx/' + encodeURIComponent(JSON.stringify(payload));
+
+	makeRequest(url, callback);
 }
 
 module.exports = pushtx;

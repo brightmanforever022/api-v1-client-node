65de73861ed2113cdc8675c1c214c3b316185c69
brightmanforever022 <brightmanforever@outlook.com>
2015-02-16 17:22:08 -0500

    Added Block Explorer Module
    blockexplorer implemented

--- blockchain.info/index.js
+++ blockchain.info/index.js
@@ -2,4 +2,5 @@ exports.MyWallet = require('./MyWallet');
 exports.CreateWallet = require('./CreateWallet');
 exports.exchangeRates = require('./exchangerates');
 exports.statistics = require('./statistics');
+exports.blockexplorer = require('./blockexplorer');
 exports.pushtx = require('./pushtx');

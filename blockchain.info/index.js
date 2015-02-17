976292e6a1b336562d53d0d1d854e95217239d37
brightmanforever022 <brightmanforever@outlook.com>
2015-02-17 17:38:24 -0500

    File rearrangement

--- blockchain.info/index.js
+++
@@ -1,7 +0,0 @@
-exports.blockexplorer = require('./blockexplorer');
-exports.CreateWallet = require('./CreateWallet');
-exports.exchangeRates = require('./exchangerates');
-exports.MyWallet = require('./MyWallet');
-exports.pushtx = require('./pushtx');
-exports.Receive = require('./Receive');
-exports.statistics = require('./statistics');

ddbfbeca8aaef00dc11ebb36cb5ed515097a3364
brightmanforever022 <brightmanforever@outlook.com>
2015-11-18 13:28:52 -0500

    Remove index reference to old Receive API

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ exports.createWallet = require('./lib/createwallet');
 exports.exchangeRates = require('./lib/exchangerates');
 exports.MyWallet = require('./lib/MyWallet');
 exports.pushtx = require('./lib/pushtx');
-exports.Receive = require('./lib/Receive');
 exports.statistics = require('./lib/statistics');

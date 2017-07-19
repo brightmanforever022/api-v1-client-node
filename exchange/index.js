e2d1845397ef604d6299c48f46ddba9d389528ee
brightmanforever022 <brightmanforever@outlook.com>
2017-07-19 13:24:02 -0400

    make time parameter optional for fromBTC, fixes #40

--- exchange/index.js
+++ exchange/index.js
@@ -3,7 +3,7 @@ var UrlPattern = require('url-pattern')
 
 var endpoints = {
   ticker: new UrlPattern('/ticker(?api_code=:apiCode)'),
-  frombtc: new UrlPattern('/frombtc?value=:value&time=:time&currency=:currency(&api_code=:apiCode)'),
+  frombtc: new UrlPattern('/frombtc?value=:value&currency=:currency(&time=:time)(&api_code=:apiCode)'),
   tobtc: new UrlPattern('/tobtc?value=:value&currency=:currency(&api_code=:apiCode)')
 }
 
@@ -23,7 +23,7 @@ function getTicker (options) {
 
 function fromBTC (amount, currency, options) {
   options = options || {}
-  return api.request('frombtc', { value: amount, time: options.time || 0, currency: currency, apiCode: options.apiCode })
+  return api.request('frombtc', { value: amount, time: options.time, currency: currency, apiCode: options.apiCode })
     .then(function (rvalue) { return parseFloat(rvalue.toString().replace(',', '')) })
 }
 
